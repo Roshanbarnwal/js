c7c610a162d518a98707dacd31359929de0aaa75
Roshan Barnwal <roshanbarnwal4374@gmail.com>
2024-08-06 12:15:42 +0000

    ss3.js modified

--- ws3/ss3.js
+++ ws3/ss3.js
@@ -6,3 +6,16 @@ console.log('99' == 99); //ToNumber('99') => 99, 99 == 99 -> true
 console.log(false == 0); //ToNumber(false) => 0, 0 == 0 -> true
 console.log(true == 10); //ToNumber(true) => 1, 1 == 10 -> false
 console.log(10 == {valueOf(){return 10}}); //ToPrimitive({valueOf(){return 10}}) => 10, 10 == 10 -> true
+/**
+ * There can be chances for same decimal numbers not to be equal because of precision. So, it is said to avoid decimal
+ * comparison if possible in binary Search of real numbers
+ */
+let a = 5;
+if(a){
+    console.log("Hi, there."); //this line will be printed
+}
+
+if(a == true){ //5 == 1 -> false
+    console.log("By by"); //this line won't be printed
+}
+//x > y; '>' applies ToPrimitive(x,hint = 'number') & ToPrimitive(y,hint='number')
