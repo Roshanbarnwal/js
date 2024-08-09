1d9025c4f82f683115ddaaf5cecf49bf48c4c9a5
Roshan Barnwal <roshanbarnwal4374@gmail.com>
2024-08-09 19:00:04 +0000

    comma in console.log explored

---
+++ ws3/ss02.js
@@ -0,0 +1,14 @@
+console.log([].toString(),[""].toString(),['r'].toString(),['0'].toString(),['o'].toString(),
+                  [undefined].toString(),{}.toString(),[null].toString());
+
+console.log("comma");
+console.log([].toString()+ "01");
+console.log([""].toString()+"02");
+console.log(['r'].toString()+"03");
+console.log(""+"010");
+console.log(['0'].toString()+"04");
+console.log(['o'].toString()+"05");
+console.log([undefined].toString()+"06");
+console.log({}.toString()+"07");
+console.log([null].toString()+"08");
+console.log(["   "].toString()+"09")
